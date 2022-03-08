8efb132d3071ff341c94412d83e764152068334f
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2022-03-08 22:51:14 +0100

    Health System

---
+++ Portfolio/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+    public int currentHealth, maxHealth;
+
+    public float invicibleLength;
+    private float invicibleCounter;
+
+    private SpriteRenderer theSr;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        currentHealth = maxHealth;
+        theSr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invicibleCounter > 0)
+        {
+            invicibleCounter -= Time.deltaTime;
+            if (invicibleCounter <= 0)
+            {
+                theSr.color = new Color(theSr.color.r, theSr.color.g, theSr.color.b, 1);
+            }
+        }
+    }
+
+    public void DealDamage()
+    {
+        if (invicibleCounter <= 0)
+        {
+            currentHealth -= 1;
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invicibleCounter = invicibleLength;
+                theSr.color = new Color(theSr.color.r, theSr.color.g, theSr.color.b, .5f);
+            }
+            UIController.instance.UpdateHealthDisplay();
+        }
+    }
+}
