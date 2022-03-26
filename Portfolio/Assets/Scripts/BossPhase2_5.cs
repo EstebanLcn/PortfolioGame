041d93669aa9185975b5f767435983397082518b
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2022-03-26 20:29:45 +0100

    ShowBars when there is a new phase

---
+++ Portfolio/Assets/Scripts/BossPhase2_5.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossPhase2_5 : StateMachineBehaviour
+{
+    private string textUI = "BOSS PHASE SOFT SKILLS";
+    GameObject originalGameObject;
+    GameObject child;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Text txt = GameObject.Find("Canvas/BossPhaseName").GetComponent<Text>();
+        txt.text = textUI;
+        originalGameObject = GameObject.Find("Canvas");
+        child = originalGameObject.transform.GetChild(8).gameObject;
+        child.SetActive(true);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Yea");
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
