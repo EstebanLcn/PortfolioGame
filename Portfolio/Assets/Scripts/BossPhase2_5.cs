151df78d720c5e6c1cb379f473d8399ae5888037
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2022-03-27 23:50:14 +0200

    Spawner soft skills

--- Portfolio/Assets/Scripts/BossPhase2_5.cs
+++ Portfolio/Assets/Scripts/BossPhase2_5.cs
@@ -8,6 +8,17 @@ public class BossPhase2_5 : StateMachineBehaviour
     GameObject child;
     GameObject childSlider;
     GameObject player;
+
+    public GameObject[] words;
+
+    private float _timeBetweenSpawn;
+    private GameObject _objectToBeDestroyed;
+    public float _startTimeBetweenSpawn;
+    private float _destroyTime = 10;
+    private int counter = 0;
+    public Transform spawnPointLeft;
+    public Transform spawnPointRight;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -19,18 +30,44 @@ public class BossPhase2_5 : StateMachineBehaviour
         Camera m_OrthographicCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         m_OrthographicCamera.orthographicSize = 7.0f;
         CameraFollow.instance.offset = new Vector3(0, 0, -10);
-        GameObject boss = GameObject.Find("Boss");
-        //boss.transform.position = new Vector3(150,0,0);
         childSlider = originalGameObject.transform.GetChild(6).gameObject;
         childSlider.SetActive(false);
         PlayerMovement2D.instance.enableY = false;
         player = GameObject.Find("Player");
         player.transform.position = new Vector3(0, 0, 0);
+        MoveBoss.instance.enableMovement = false;
+        GameObject boss = GameObject.Find("Boss");
+        boss.transform.position = new Vector3(150, 0, 0);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (_timeBetweenSpawn <= 0)
+        {
+            if(counter % 2 == 0)
+            {
+                _objectToBeDestroyed = Instantiate(words[counter], new Vector2(spawnPointRight.position.x, spawnPointRight.position.y), Quaternion.identity);
+                _timeBetweenSpawn = _startTimeBetweenSpawn;
+                counter++;
+            }
+            else
+            {
+                _objectToBeDestroyed = Instantiate(words[counter], new Vector2(spawnPointLeft.position.x, spawnPointLeft.position.y), Quaternion.identity);
+                _timeBetweenSpawn = _startTimeBetweenSpawn;
+                counter++;
+            }
+            
+        }
+        else
+        {
+            _timeBetweenSpawn -= Time.deltaTime;
+        }
+        Destroy(_objectToBeDestroyed, _destroyTime);
+        if (counter == 6)
+        {
+            counter = 0;
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
--- Portfolio/Assets/Scripts/MoveBoss.cs
+++ Portfolio/Assets/Scripts/MoveBoss.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class MoveBoss : MonoBehaviour
 {
+    public static MoveBoss instance;
     private float moveSpeed = 4.5f;
     private bool facingTop = true;
     Vector3 pos, localScale;
+    public bool enableMovement = true;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
         pos = transform.position;
@@ -38,12 +45,18 @@ public class MoveBoss : MonoBehaviour
 
     void MoveTop()
     {
-        pos += transform.up * Time.deltaTime * moveSpeed;
-        transform.position = pos;
+        if (enableMovement)
+        {
+            pos += transform.up * Time.deltaTime * moveSpeed;
+            transform.position = pos;
+        }
     }
     void MoveBot()
     {
-        pos -= transform.up * Time.deltaTime * moveSpeed;
-        transform.position = pos;
+        if (enableMovement)
+        {
+            pos -= transform.up * Time.deltaTime * moveSpeed;
+            transform.position = pos;
+        }
     }
 }
